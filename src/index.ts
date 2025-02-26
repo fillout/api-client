9299ffad36597f793af19f5208fb55ffdfb3dd93
xkcdstickfigure <97917457+xkcdstickfigure@users.noreply.github.com>
2025-02-26 02:01:53 +0000

    add files

---
+++ src/index.ts
@@ -0,0 +1,247 @@
+import { FilloutForm, FilloutSubmission } from "./types.js";
+
+export class Fillout {
+  private token: string;
+  private baseUrl: string;
+
+  constructor(token: string, options?: { region?: "us" | "eu" | "ca" }) {
+    if (typeof token !== "string" || !token.startsWith("sk_prod_")) {
+      throw new Error(
+        "Invalid Fillout API key. Visit https://build.fillout.com/home/settings/developer to create one."
+      );
+    }
+
+    const region = options?.region || "us";
+
+    this.token = token;
+    this.baseUrl =
+      region === "eu"
+        ? "https://eu-api.fillout.com"
+        : region === "ca"
+          ? "https://ca-api.fillout.com"
+          : "https://api.fillout.com";
+  }
+
+  /** Get all the forms in your organization */
+  getForms = async () => {
+    const res = await fetch(`${this.baseUrl}/v1/api/forms`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    });
+    if (!res.ok) throw await filloutError("fetch Fillout forms", res);
+
+    const data: { formId: string; name: string }[] = await res.json();
+    return data;
+  };
+
+  /**
+   * Get the metadata of a form
+   * @param formId - The id found in the URL of a form, eg. `foAdHjd1Duus`
+   */
+  getForm = async (formId: string) => {
+    const res = await fetch(
+      `${this.baseUrl}/v1/api/forms/${encodeURIComponent(formId)}`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      }
+    );
+    if (!res.ok)
+      throw await filloutError(`fetch Fillout form '${formId}'`, res);
+
+    const data: FilloutForm = await res.json();
+    return data;
+  };
+
+  /**
+   * Get form submissions
+   * @param formId - The id found in the URL of a form, eg. `foAdHjd1Duus`
+   */
+  getSubmissions = async (
+    formId: string,
+    options?: {
+      /** The maximum number of submissions to retrieve per request. Must be a number between 1 and 150. Default is 50. */
+      limit?: number;
+      /** A date string to filter submissions submitted after this date */
+      afterDate?: string;
+      /** A date string to filter submissions submitted before this date */
+      beforeDate?: string;
+      /** The starting position from which to fetch the submissions. Default is 0. */
+      offset?: number;
+      /** Pass `in_progress` to get a list of in-progress (unfinished) submissions. By default, only `finished` submissions are returned. Note that fetching in progress submissions is available starting on the business plan. */
+      status?: "in_progress" | "finished";
+      /** Pass `true` to include a link to edit the submission as `editLink` */
+      includeEditLink?: boolean;
+      /** Pass `true` to include preview responses */
+      includePreview?: boolean;
+      /** Can be `asc` or `desc`, defaults to `asc` */
+      sort?: "asc" | "desc";
+      /** Filter for submissions containing a string of text */
+      search?: string;
+    }
+  ) => {
+    const res = await fetch(
+      `${this.baseUrl}/v1/api/forms/${encodeURIComponent(formId)}/submissions?${toURLSearchParams(options || {})}`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      }
+    );
+    if (!res.ok) {
+      throw await filloutError(
+        `fetch submissions for Fillout form '${formId}'`,
+        res
+      );
+    }
+
+    const data: {
+      responses: FilloutSubmission[];
+      totalResponses: number;
+      pageCount: number;
+    } = await res.json();
+    return data;
+  };
+
+  /**
+   * Get form submission by id
+   * @param formId - The id found in the URL of a form, eg. `foAdHjd1Duus`
+   * @param submissionId - The uuid of a submission
+   */
+  getSubmission = async (
+    formId: string,
+    submissionId: string,
+    options?: {
+      /** Pass `true` to include a link to edit the submission as `editLink` */
+      includeEditLink?: boolean;
+    }
+  ) => {
+    const res = await fetch(
+      `${this.baseUrl}/v1/api/forms/${encodeURIComponent(formId)}/submissions/${encodeURIComponent(submissionId)}?${toURLSearchParams(options || {})}`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      }
+    );
+    if (!res.ok) {
+      throw await filloutError(
+        `fetch Fillout submission '${submissionId}'`,
+        res
+      );
+    }
+
+    const data: { submission: FilloutSubmission } = await res.json();
+    return data.submission;
+  };
+
+  /**
+   * Delete a form submission by id
+   * @param formId - The id found in the URL of a form, eg. `foAdHjd1Duus`
+   * @param submissionId - The uuid of a submission
+   */
+  deleteSubmission = async (formId: string, submissionId: string) => {
+    const res = await fetch(
+      `${this.baseUrl}/v1/api/forms/${encodeURIComponent(formId)}/submissions/${encodeURIComponent(submissionId)}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      }
+    );
+    if (!res.ok) {
+      throw await filloutError(
+        `delete Fillout submission '${submissionId}'`,
+        res
+      );
+    }
+  };
+
+  /**
+   * Create a webhook for form submissions
+   * @param formId - The id found in the URL of a form, eg. `foAdHjd1Duus`
+   * @param url - The endpoint where you’d like to listen for submissions
+   */
+  createWebhook = async (formId: string, url: string) => {
+    const res = await fetch(`${this.baseUrl}/v1/api/webhook/create`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ formId, url }),
+    });
+    if (!res.ok) throw await filloutError(`create Fillout webhook`, res);
+
+    const data: { id: number } = await res.json();
+    return data;
+  };
+
+  /**
+   * Remove a webhook
+   * @param webhookId - The numerical ID you received when creating the webhook
+   */
+  deleteWebhook = async (webhookId: number) => {
+    const res = await fetch(`${this.baseUrl}/v1/api/webhook/delete`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ webhookId }),
+    });
+    if (!res.ok) {
+      throw await filloutError(`delete Fillout webhook ${webhookId}`, res);
+    }
+  };
+
+  /**
+   * Import submissions
+   * @param formId - The id found in the URL of a form, eg. `foAdHjd1Duus`
+   * @param submissions - The maximum number of submissions that can be imported per request is 10.
+   *
+   * See also: https://www.fillout.com/help/fillout-rest-api#951dc79e6b8b476aa808ea670252609d
+   */
+  importSubmissions = async (
+    formId: string,
+    submissions: Record<string, any>[]
+  ) => {
+    const res = await fetch(
+      `${this.baseUrl}/v1/api/forms/${encodeURIComponent(formId)}/submissions`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ submissions }),
+      }
+    );
+
+    if (!res.ok) throw await filloutError(`import Fillout submissions`, res);
+
+    const data: { id: number } = await res.json();
+    return data;
+  };
+}
+
+const filloutError = async (verb: string, res: Response) => {
+  const ct = res.headers.get("Content-Type")?.split(";")[0];
+  if (ct === "application/json") {
+    const body = await res.json();
+    if (body.message) return new Error(body.message);
+  }
+
+  return new Error(`Failed to ${verb} with status code ${res.status}`);
+};
+
+const toURLSearchParams = (body: Record<string, unknown>) => {
+  const params = new URLSearchParams();
+  Object.entries(body).forEach(([key, value]) => {
+    if (value) params.set(key, String(value));
+  });
+  return params;
+};
---
+++ src/types.ts
@@ -0,0 +1,163 @@
+import { FILLOUT_QUESTION_TYPES } from "./constants.js";
+
+export type FilloutForm = {
+  id: string;
+  name: string;
+  questions: FilloutQuestion[];
+  calculations: FilloutCalculation[];
+  urlParameters: { id: string; name: string }[];
+  documents: { id: string; name: string }[];
+  scheduling: { id: string; name: string }[];
+  payments: { id: string; name: string }[];
+  approvals?: FilloutApproval[];
+};
+
+type FilloutQuestion = {
+  id: string;
+  name: string;
+  type: FilloutQuestionType;
+  options?: FilloutOption[];
+};
+
+type FilloutQuestionType = (typeof FILLOUT_QUESTION_TYPES)[number];
+
+type FilloutOption = {
+  id: string;
+  value: string;
+  label: string;
+};
+
+type FilloutCalculation = {
+  id: string;
+  name: string;
+  type: FilloutCalculationType;
+};
+
+type FilloutCalculationType = "text" | "number" | "duration";
+
+type FilloutApproval = {
+  workflowId: string;
+  nodeId: string;
+  approvers: { userId: number }[];
+  states: FilloutApprovalState[];
+  inputFields: FilloutApprovalInputField[];
+};
+
+type FilloutApprovalState = {
+  id: string;
+  name: string;
+  color: string;
+  nextNodeId: string | undefined;
+};
+
+type FilloutApprovalInputField = {
+  customFieldId: string;
+  label: string;
+  required: boolean;
+  showForStates: {
+    type: "all" | "custom";
+    customStateIds: string[];
+  };
+};
+
+export type FilloutSubmission = {
+  submissionId: string;
+  submissionTime: string;
+  lastUpdatedAt: string;
+  startedAt?: string;
+  questions: {
+    id: string;
+    name: string;
+    type: FilloutQuestionType;
+    value: any; // TODO
+  }[];
+  calculations: {
+    id: string;
+    name: string;
+    type: FilloutCalculationType;
+    value: string | number | null;
+  }[];
+  quiz:
+    | {
+        score: number | undefined;
+        maxScore: number | undefined;
+      }
+    | Record<string, never>;
+  documents: {
+    id: string;
+    name: string;
+    url: string;
+  }[];
+  scheduling: {
+    id: string;
+    name: string;
+    value: FilloutSchedulingValue | null;
+  }[];
+  payments: {
+    id: string;
+    name: string;
+    value: FilloutPaymentValue | null;
+  }[];
+  urlParameters: {
+    id: string;
+    name: string;
+    value: string | null;
+  }[];
+  approvals?: FilloutApprovalModel[];
+  editLink: string | undefined;
+  login?: {
+    email: string;
+  };
+};
+
+type FilloutSchedulingValue = {
+  fullName: string;
+  email: string;
+  phone: string;
+  timezone: string;
+  eventStartTime: string;
+  eventEndTime: string;
+  eventId: string;
+  eventUrl: string;
+  rescheduleOrCancelUrl?: string;
+  scheduledUserEmail?: string;
+};
+
+type FilloutPaymentValue = {
+  discountCount: string | undefined;
+  stripeCustomerId: string | undefined;
+  stripeCustomerUrl: string | undefined;
+  stripePaymentUrl: string | undefined;
+  totalAmount: number | undefined;
+  currency: string | undefined;
+  email: string | undefined;
+  payment: string | undefined;
+  status: FilloutPaymentStatus;
+  stripeSubscriptionId: string | undefined;
+};
+
+type FilloutPaymentStatus =
+  | "requires_payment_method"
+  | "requires_confirmation"
+  | "requires_action"
+  | "processing"
+  | "requires_capture"
+  | "canceled"
+  | "succeeded"
+  | ""
+  | "succeeded_free"
+  | "trial_created";
+
+type FilloutApprovalModel = {
+  workflowId: string;
+  workflowRunId: string;
+  nodeId: string;
+  decisions: FilloutApprovalDecision[];
+  finalStateId: string | undefined;
+};
+
+type FilloutApprovalDecision = {
+  userId: number;
+  selectedStateId: string;
+  createdAt: string;
+};
